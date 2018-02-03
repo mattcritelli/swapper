1c8a171a06c4886cfc43ca0167bd655c93a9e07b
Matt C <matthew.critelli@gmail.com>
2018-02-03 12:53:59 -0700

    Wire up routing for add new workspace apge with form.

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var workspaces = [
+  {name: 'Denver Apartment', image: "https://farm4.staticflickr.com/3146/2320148152_baee347d9f.jpg"},
+  {name: 'Denver Workshare Hot Seat', image: "https://www.modworks.com/sites/default/files/styles/horizontal_block_no_overlay/public/h_block/image/modworks-hot-seats.jpg?itok=RJeWaB0l"},
+  {name: 'NYC Workshare Dedicated Desk', image: "https://www.modworks.com/sites/default/files/styles/horizontal_block_no_overlay/public/h_block/image/modworks-dedicated-desks.jpg?itok=vlXvKyMH"}
+]
 
 app.get("/", function(req, res){
   res.render("landing")
 });
 
 app.get("/workspaces", function(req,res){
-  var workspaces = [
-    {name: 'Denver Apartment', image: "https://farm4.staticflickr.com/3146/2320148152_baee347d9f.jpg"},
-    {name: 'Denver Workshare Hot Seat', image: "https://www.modworks.com/sites/default/files/styles/horizontal_block_no_overlay/public/h_block/image/modworks-hot-seats.jpg?itok=RJeWaB0l"},
-    {name: 'NYC Workshare Dedicated Desk', image: "https://www.modworks.com/sites/default/files/styles/horizontal_block_no_overlay/public/h_block/image/modworks-dedicated-desks.jpg?itok=vlXvKyMH"}
-  ]
-
   res.render("workspaces", {"workspaces": workspaces});
-
 });
+
+app.post("/workspaces", function(req, res){
+  var name = req.body.name
+  var image = req.body.image
+  var newWorkspace = {name: name, image: image}
+  workspaces.push(newWorkspace)
+  res.redirect("/workspaces")
+});
+
+app.get("/workspaces/new", function(req, res){
+  res.render("new")
+})
 
 app.listen(3000, function(){
   console.log("Swapper has started...");
