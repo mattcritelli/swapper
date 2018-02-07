a43d320f6c6e85b634628b55e93e0fa28fd628e0
Matt C <matthew.critelli@gmail.com>
2018-02-07 14:12:54 -0700

    Add ability to edit and delete workspaces. Refactor inline form styling to css.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     Workspace   = require("./models/workspace"),
     seedDB      = require("./seeds"),
     Review      = require("./models/review"),
@@ -16,9 +17,10 @@ var workspaceRoutes = require("./routes/workspaces"),
 
 mongoose.connect("mongodb://localhost/swapper");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
-seedDB();
+// seedDB();
 
 // ==== PASSPORT CONFIGURATION ====
 app.use(require("express-session")({
@@ -35,13 +37,13 @@ passport.deserializeUser(User.deserializeUser());
 // ==== ROUTE PROTECTION ====
 // Store currentUser on req.locals as local variable
 app.use(function(req, res, next){
-  res.locals.currentUser = req.user
-  next()
+  res.locals.currentUser = req.user;
+  next();
 })
 
 app.use("/", indexRoutes)
-app.use("/workspaces", workspaceRoutes)
-app.use("/workspaces/:id/reviews", reviewRoutes)
+app.use("/workspaces", workspaceRoutes);
+app.use("/workspaces/:id/reviews", reviewRoutes);
 
 app.listen(3000, function(){
   console.log("Swapper has started...");
